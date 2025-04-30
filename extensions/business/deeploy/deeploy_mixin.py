ada28394637eed1fc44153798e2c9919d7828475
vitalii-t12 <vitalii.toderian@gmail.com>
2025-04-30 13:32:50 +0300

    fix: rename check function

--- extensions/business/deeploy/deeploy_mixin.py
+++ extensions/business/deeploy/deeploy_mixin.py
@@ -178,7 +178,7 @@ class _DeeployMixin:
     # endfor each target node
     return response_keys
 
-  def __monitor_pipeline_responses(self, response_keys, timeout_seconds=300):
+  def __get_pipeline_responses(self, response_keys, timeout_seconds=60):
     """
     Wait until all the responses are received via CSTORE and compose status response.
     Args:
@@ -367,8 +367,10 @@ class _DeeployMixin:
     # Phase 2: Launch the pipeline on each node and set CSTORE `response_key`` for the "callback" action
     response_keys = self.__launch_pipeline_on_nodes(nodes, inputs, app_id, app_alias, app_type, sender)
 
-    # Phase 3: Wait until all the responses are received via CSTORE and compose status response (TODO: method)
-    dct_status, str_status = self.__monitor_pipeline_responses(response_keys)
+    # Phase 3: Wait until all the responses are received via CSTORE and compose status response
+    dct_status, str_status = self.__get_pipeline_responses(response_keys)
+
+    self.P(f"Pipeline responses: str_status = {str_status} | dct_status = {self.json_dumps(dct_status)}")
 
     # TODO: we must define failure and success conditions (after initial implementation is done)
 
