deba88781e9cca582616dfba9298b9f5f4e15a73
vitalii-t12 <vitalii.toderian@gmail.com>
2025-07-24 15:13:08 +0300

    fix: remove dead code from cstore and r1fs apis

--- extensions/business/r1fs/r1fs_manager_api.py
+++ extensions/business/r1fs/r1fs_manager_api.py
@@ -36,63 +36,6 @@ class R1fsManagerApiPlugin(BasePlugin):
       self.__class__.__name__, my_address, my_eth_address,
     ))
     return
-  
-  def __get_current_epoch(self):
-    """
-    Get the current epoch of the node.
-
-    Returns
-    -------
-    int
-        The current epoch of the node.
-    """
-    return self.netmon.epoch_manager.get_current_epoch()
-  
-   
-  
-  def __sign(self, data):
-    """
-    Sign the given data using the blockchain engine.
-    Returns the signature. 
-    Use the data param as it will be modified in place.
-    """
-    signature = self.bc.sign(data, add_data=True, use_digest=True)
-    return signature
-
-  def __get_response(self, dct_data: dict):
-    """
-    Create a response dictionary with the given data.
-
-    Parameters
-    ----------
-    dct_data : dict
-        The data to include in the response - data already prepared 
-
-    Returns
-    -------
-    dict
-        The input dictionary with the following keys added:
-        - server_alias: str
-            The literal alias of the current node.
-
-        - server_time: str
-            The current time in UTC of the current node.
-
-        - server_current_epoch: int
-            The current epoch of the current node.
-
-        - server_uptime: str
-            The time that the current node has been running.
-    """
-    str_utc_date = self.datetime.now(self.timezone.utc).strftime('%Y-%m-%d %H:%M:%S')
-    # dct_data['server_id'] = self.node_addr # redundant due to the EE_SENDER
-    dct_data['server_alias'] = self.node_id
-    dct_data['server_version'] = self.ee_ver
-    dct_data['server_time'] = str_utc_date
-    dct_data['server_current_epoch'] = self.__get_current_epoch()
-    dct_data['server_uptime'] = str(self.timedelta(seconds=int(self.time_alive)))
-    self.__sign(dct_data) # add the signature over full data
-    return dct_data
 
 
   @BasePlugin.endpoint(method="get", require_token=False)
