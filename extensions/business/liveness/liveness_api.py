7858065a8bdcd7f11d6cee1b89778e6336035314
Andrei Ionut Damian <andrei.damian@me.com>
2025-08-13 15:36:16 +0300

    fix: basic ver

--- extensions/business/liveness/liveness_api.py
+++ extensions/business/liveness/liveness_api.py
@@ -1,11 +1,18 @@
 
+from unittest import result
 from naeural_core.business.default.web_app.supervisor_fast_api_web_app import SupervisorFastApiWebApp as BasePlugin
+from ratio1.const.evm_net import EvmNetData, EVM_NET_DATA
 
-__VER__ = '0.3.3'
+__VER__ = '0.1.0'
 
 _CONFIG = {
   **BasePlugin.CONFIG,
 
+  "MONITORED_SERVICES" : {
+      "DAUTH-API": EvmNetData.DAUTH_URL_KEY,
+      "ORACLE-API": EvmNetData.EE_ORACLE_API_URL_KEY,
+      "DEEPLOY-API": EvmNetData.EE_DEEPLOY_API_URL_KEY
+    },
   'PORT': None,
   # 'ASSETS': 'plugins/business/fastapi/epoch_manager',
   'VALIDATION_RULES': {
@@ -28,84 +35,75 @@ class LivenessApiPlugin(BasePlugin):
 
   def on_init(self):
     super(LivenessApiPlugin, self).on_init()
-
-    current_epoch = self.__get_current_epoch()
+    self.const.BASE_CT.dAuth
+    current_epoch = self.netmon.epoch_manager.get_current_epoch()
     self.P("Started {} plugin in epoch {}".format(
       self.__class__.__name__, current_epoch)
     )
-    # TODO: Bleo lock it until we `can_serve` it
     return
-
-
-  def __sign(self, data):
-    """
-    Sign the given data using the blockchain engine.
-    Returns the signature. 
-    Use the data param as it will be modified in place.
-    """
-    signature = self.bc.sign(data, add_data=True, use_digest=True)
-    return signature
-
-
-  def __get_response(self, dct_data: dict, **kwargs):
-    """
-    Create a response dictionary with the given data.
-
-    Parameters
-    ----------
-    dct_data : dict
-        The data to include in the response - data already prepared 
-
-    Returns
-    -------
-    dict
-        The input dictionary with the following keys added:
-        - server_alias: str
-            The literal alias of the current node.
-
-        - server_time: str
-            The current time in UTC of the current node.
-
-        - server_current_epoch: int
-            The current epoch of the current node.
-
-        - server_uptime: str
-            The time that the current node has been running.
-    """
-    str_utc_date = self.datetime.now(self.timezone.utc).strftime('%Y-%m-%d %H:%M:%S')
-    # dct_data['server_id'] = self.node_addr # redundant due to the EE_SENDER
-    dct_data['server_alias'] = self.node_id
-    dct_data['server_version'] = self.ee_ver
-    dct_data['server_time'] = str_utc_date
-    dct_data['server_current_epoch'] = self.__get_current_epoch()
-    dct_data['server_last_synced_epoch'] = self.__get_synced_epoch()
-    dct_data['server_uptime'] = str(self.timedelta(seconds=int(self.time_alive)))
-    for k, v in kwargs.items():
-      # some filters may be applied to the data
-      dct_data[k] = v
-    # end for kwargs
-    self.__sign(dct_data) # add the signature over full data
-    return dct_data
-
-  def __get_current_epoch(self):
-    """
-    Get the current epoch of the node.
-
-    Returns
-    -------
-    int
-        The current epoch of the node.
-    """
-    real_epoch = self.netmon.epoch_manager.get_current_epoch()
-    return real_epoch  
   
+
+  def __get_service_url_mapping(self, service_key=''):
+    dct_mapping = self.cfg_monitored_services
+    url_key = dct_mapping.get(service_key.upper())
+    evm_net = self.bc.get_evm_network()
+    evm_net_data = EVM_NET_DATA.get(evm_net, {})
+    url = evm_net_data.get(url_key, None)
+    return url
+
+
+  def __get_service_status(self, url=''):
+    """
+    Uses self.requests to test if the url is a live swagger-based api
+    """
+    if url is None:
+      return {"error": "Service URL is not available."}
+  
+    response = self.requests.get(url, timeout=5)
+    if response.status_code == 200:
+      return {"status": "running"}
+    else:
+      return {"status": "stopped"}
+    
+    
+  def __get_all_services_statuses(self):
+    statuses = {}
+    for service in self.cfg_monitored_services.keys():
+      url = self.__get_service_url_mapping(service)
+      statuses[service] = self.__get_service_status(url)
+    return statuses
+
   @BasePlugin.endpoint
   # /get_liveness
   def get_liveness(self, service=None):
     """
+    
+    Parameters
+    ----------
+
+    service: str
+        The name of the service to check the liveness for:
+          "DAUTH-API"
+          "ORACLE-API"
+          "DEEPLOY-API"
 
     """
-    data = {}
+    try:
+      if service is None:
+        data = self.__get_all_services_statuses()
+      else:
+        data = {}
+        url = self.__get_service_url_mapping(service)
+        if url is None:
+          data['error'] = f"Service '{service}' is not recognized or not available."
+        else:
+          data[service.upper()] = self.__get_service_status(url)
+        #endif
+    except Exception as e:
+      data = {'error' : str(e)}
+    #endtry
     
-    response = self.__get_response(data)
+    response = self._get_response({
+      'services' : {**data}
+    })
     return response
