d83bae6c8354f396c7a81a63d4eac5b5a994f39b
Alessandro <defranceschi_a@yahoo.com>
2026-05-29 14:05:47 +0200

    Migrate TCP tunnels to port registry

--- extensions/business/tunnels/tests/test_tunnels_manager_cloudflare_errors.py
+++ extensions/business/tunnels/tests/test_tunnels_manager_cloudflare_errors.py
@@ -1,6 +1,8 @@
 import sys
 import types
 import unittest
+from copy import deepcopy
+from unittest import mock
 
 
 _supervisor_module = types.ModuleType("naeural_core.business.default.web_app.supervisor_fast_api_web_app")
@@ -36,13 +38,15 @@ class _ResponseStub:
 
 class _RequestsStub:
 
-  def __init__(self, *, post_payloads=None, patch_payloads=None, delete_payloads=None):
+  def __init__(self, *, post_payloads=None, patch_payloads=None, delete_payloads=None, get_payloads=None):
     self.post_payloads = list(post_payloads or [])
     self.patch_payloads = list(patch_payloads or [])
     self.delete_payloads = list(delete_payloads or [])
+    self.get_payloads = list(get_payloads or [])
     self.posts = []
     self.patches = []
     self.deletes = []
+    self.gets = []
 
   def post(self, url, headers=None, json=None):
     self.posts.append({"url": url, "headers": headers, "json": json})
@@ -56,15 +60,63 @@ class _RequestsStub:
     self.deletes.append({"url": url, "headers": headers})
     return _ResponseStub(self.delete_payloads.pop(0))
 
+  def get(self, url, headers=None):
+    self.gets.append({"url": url, "headers": headers})
+    return _ResponseStub(self.get_payloads.pop(0))
+
 
 def make_plugin(requests):
   plugin = TunnelsManagerPlugin.__new__(TunnelsManagerPlugin)
   plugin.requests = requests
   plugin.cfg_base_cloudflare_url = "https://api.cloudflare.com"
-  plugin.cfg_tcp_prefix = "cft"
   plugin.cfg_tcp_proxy_url = "tcp.ratio1.link"
+  plugin.cfg_tcp_routes_hkey = "tcp_routes"
+  plugin.cfg_tcp_public_port_range_start = 30000
+  plugin.cfg_tcp_public_port_range_end = 39999
   plugin.uuid = lambda: "uuid-001"
+  plugin.time = lambda: 1000
+  plugin.time_to_str = lambda value: f"time-{value}"
+  plugin.deepcopy = deepcopy
+  plugin.P = lambda *args, **kwargs: None
+  plugin._chainstore = {}
+  plugin._chainstore_hsets = []
+
+  def chainstore_hsync(**kwargs):
+    return {"merged_fields": 0}
+
+  def chainstore_hgetall(hkey, **kwargs):
+    return deepcopy(plugin._chainstore.get(hkey, {}))
+
+  def chainstore_hget(hkey, key, **kwargs):
+    value = plugin._chainstore.get(hkey, {}).get(str(key))
+    return deepcopy(value)
+
+  def chainstore_hset(hkey, key, value, readonly=False, **kwargs):
+    plugin._chainstore_hsets.append({"hkey": hkey, "key": str(key), "value": deepcopy(value), "readonly": readonly})
+    store = plugin._chainstore.setdefault(hkey, {})
+    key = str(key)
+    if value is None:
+      store.pop(key, None)
+      return True
+    if readonly and key in store and store[key] != value:
+      return False
+    store[key] = deepcopy(value)
+    return True
+
+  plugin.chainstore_hsync = chainstore_hsync
+  plugin.chainstore_hgetall = chainstore_hgetall
+  plugin.chainstore_hget = chainstore_hget
+  plugin.chainstore_hset = chainstore_hset
   return plugin
+
+
+class _FixedRandom:
+
+  def __init__(self, ordered_ports):
+    self.ordered_ports = ordered_ports
+
+  def shuffle(self, candidates):
+    candidates[:] = self.ordered_ports
 
 
 class TunnelsManagerCloudflareErrorTests(unittest.TestCase):
@@ -170,6 +222,365 @@ class TunnelsManagerCloudflareErrorTests(unittest.TestCase):
     self.assertIn("Invalid tunnel metadata.", message)
     self.assertNotIn("NoneType", message)
     self.assertEqual(len(requests.deletes), 2)
+
+  def test_tcp_tunnel_creation_allocates_chainstore_route_without_public_cname(self):
+    requests = _RequestsStub(
+      post_payloads=[
+        {
+          "success": True,
+          "result": {
+            "id": "tunnel-id",
+            "token": "tunnel-token",
+          },
+        },
+        {
+          "success": True,
+          "result": {
+            "id": "dns-record-id",
+          },
+        },
+      ],
+      patch_payloads=[
+        {
+          "success": True,
+          "result": {
+            "id": "tunnel-id",
+            "metadata": {},
+          },
+        },
+      ],
+    )
+    plugin = make_plugin(requests)
+    plugin.cfg_tcp_public_port_range_start = 30000
+    plugin.cfg_tcp_public_port_range_end = 30000
+
+    result = plugin.new_tunnel(
+      alias="My TCP Tunnel",
+      cloudflare_account_id="account-id",
+      cloudflare_zone_id="zone-id",
+      cloudflare_api_key="api-key",
+      cloudflare_domain="ratio1.link",
+      tunnel_type="tcp",
+    )
+
+    self.assertEqual(len(requests.posts), 2)
+    self.assertEqual(requests.posts[1]["json"]["name"], "uuid-001")
+    self.assertEqual(result["tcp_route"]["public_port"], 30000)
+    self.assertEqual(result["tcp_route"]["hostname"], "uuid-001.ratio1.link")
+    self.assertEqual(result["metadata"]["alias"], "My TCP Tunnel")
+    self.assertEqual(result["metadata"]["dns_name"], "uuid-001.ratio1.link")
+    self.assertEqual(plugin.get_tcp_route(30000), "uuid-001.ratio1.link")
+    self.assertEqual(requests.patches[0]["json"]["metadata"]["tcp_public_endpoint"], "tcp.ratio1.link:30000")
+
+  def test_tcp_port_allocation_skips_occupied_ports(self):
+    requests = _RequestsStub(
+      post_payloads=[
+        {
+          "success": True,
+          "result": {
+            "id": "tunnel-id",
+            "token": "tunnel-token",
+          },
+        },
+        {
+          "success": True,
+          "result": {
+            "id": "dns-record-id",
+          },
+        },
+      ],
+      patch_payloads=[
+        {
+          "success": True,
+          "result": {
+            "id": "tunnel-id",
+            "metadata": {},
+          },
+        },
+      ],
+    )
+    plugin = make_plugin(requests)
+    plugin.cfg_tcp_public_port_range_start = 30000
+    plugin.cfg_tcp_public_port_range_end = 30001
+    plugin._chainstore[plugin.cfg_tcp_routes_hkey] = {
+      "30000": {
+        "public_port": 30000,
+        "hostname": "used.ratio1.link",
+        "tunnel_id": "other-tunnel",
+        "enabled": True,
+      }
+    }
+
+    with mock.patch(
+      "extensions.business.tunnels.tunnels_manager.random.SystemRandom",
+      return_value=_FixedRandom([30000, 30001]),
+    ):
+      result = plugin.new_tunnel(
+        alias="My TCP Tunnel",
+        cloudflare_account_id="account-id",
+        cloudflare_zone_id="zone-id",
+        cloudflare_api_key="api-key",
+        cloudflare_domain="ratio1.link",
+        tunnel_type="tcp",
+      )
+
+    self.assertEqual(result["tcp_route"]["public_port"], 30001)
+
+  def test_tcp_port_allocation_retries_after_readback_collision(self):
+    requests = _RequestsStub(
+      post_payloads=[
+        {
+          "success": True,
+          "result": {
+            "id": "tunnel-id",
+            "token": "tunnel-token",
+          },
+        },
+        {
+          "success": True,
+          "result": {
+            "id": "dns-record-id",
+          },
+        },
+      ],
+      patch_payloads=[
+        {
+          "success": True,
+          "result": {
+            "id": "tunnel-id",
+            "metadata": {},
+          },
+        },
+      ],
+    )
+    plugin = make_plugin(requests)
+    plugin.cfg_tcp_public_port_range_start = 30000
+    plugin.cfg_tcp_public_port_range_end = 30001
+    original_hset = plugin.chainstore_hset
+    hset_calls = []
+
+    def colliding_hset(hkey, key, value, readonly=False, **kwargs):
+      hset_calls.append(key)
+      if value is not None and key == "30000":
+        plugin._chainstore.setdefault(hkey, {})[key] = {
+          "public_port": 30000,
+          "hostname": "other.ratio1.link",
+          "tunnel_id": "other-tunnel",
+          "enabled": True,
+        }
+        return True
+      return original_hset(hkey=hkey, key=key, value=value, readonly=readonly, **kwargs)
+
+    plugin.chainstore_hset = colliding_hset
+
+    with mock.patch(
+      "extensions.business.tunnels.tunnels_manager.random.SystemRandom",
+      return_value=_FixedRandom([30000, 30001]),
+    ):
+      result = plugin.new_tunnel(
+        alias="My TCP Tunnel",
+        cloudflare_account_id="account-id",
+        cloudflare_zone_id="zone-id",
+        cloudflare_api_key="api-key",
+        cloudflare_domain="ratio1.link",
+        tunnel_type="tcp",
+      )
+
+    self.assertEqual(hset_calls[:2], ["30000", "30001"])
+    self.assertEqual(result["tcp_route"]["public_port"], 30001)
+
+  def test_tcp_port_allocation_exhaustion_cleans_partial_cloudflare_resources(self):
+    requests = _RequestsStub(
+      post_payloads=[
+        {
+          "success": True,
+          "result": {
+            "id": "tunnel-id",
+            "token": "tunnel-token",
+          },
+        },
+        {
+          "success": True,
+          "result": {
+            "id": "dns-record-id",
+          },
+        },
+      ],
+      delete_payloads=[
+        {
+          "success": True,
+          "result": {},
+        },
+        {
+          "success": True,
+          "result": {},
+        },
+      ],
+    )
+    plugin = make_plugin(requests)
+    plugin.cfg_tcp_public_port_range_start = 30000
+    plugin.cfg_tcp_public_port_range_end = 30000
+    plugin._chainstore[plugin.cfg_tcp_routes_hkey] = {
+      "30000": {
+        "public_port": 30000,
+        "hostname": "used.ratio1.link",
+        "tunnel_id": "other-tunnel",
+        "enabled": True,
+      }
+    }
+
+    with self.assertRaises(Exception) as ctx:
+      plugin.new_tunnel(
+        alias="My TCP Tunnel",
+        cloudflare_account_id="account-id",
+        cloudflare_zone_id="zone-id",
+        cloudflare_api_key="api-key",
+        cloudflare_domain="ratio1.link",
+        tunnel_type="tcp",
+      )
+
+    self.assertIn("No available TCP public ports", str(ctx.exception))
+    self.assertEqual(len(requests.deletes), 2)
+
+  def test_delete_tcp_tunnel_removes_chainstore_route_from_metadata_hint(self):
+    requests = _RequestsStub(
+      get_payloads=[
+        {
+          "success": True,
+          "result": {
+            "id": "tunnel-id",
+            "metadata": {
+              "dns_record_id": "dns-record-id",
+              "custom_hostnames": [],
+              "type": "tcp",
+              "tcp_public_port": 30000,
+            },
+          },
+        },
+      ],
+      delete_payloads=[
+        {
+          "success": True,
+          "result": {},
+        },
+        {
+          "success": True,
+          "result": {},
+        },
+      ],
+    )
+    plugin = make_plugin(requests)
+    plugin._chainstore[plugin.cfg_tcp_routes_hkey] = {
+      "30000": {
+        "public_port": 30000,
+        "hostname": "uuid-001.ratio1.link",
+        "tunnel_id": "tunnel-id",
+        "enabled": True,
+      }
+    }
+
+    result = plugin.delete_tunnel(
+      tunnel_id="tunnel-id",
+      cloudflare_account_id="account-id",
+      cloudflare_zone_id="zone-id",
+      cloudflare_api_key="api-key",
+    )
+
+    self.assertTrue(result["success"])
+    self.assertNotIn("30000", plugin._chainstore[plugin.cfg_tcp_routes_hkey])
+    self.assertEqual(len(requests.deletes), 2)
+    self.assertEqual(plugin._chainstore_hsets[-1]["key"], "30000")
+    self.assertIsNone(plugin._chainstore_hsets[-1]["value"])
+
+  def test_delete_tcp_route_missing_or_owned_by_other_tunnel_is_not_deleted(self):
+    requests = _RequestsStub()
+    plugin = make_plugin(requests)
+    plugin._chainstore[plugin.cfg_tcp_routes_hkey] = {
+      "30000": {
+        "public_port": 30000,
+        "hostname": "other.ratio1.link",
+        "tunnel_id": "other-tunnel",
+        "enabled": True,
+      }
+    }
+
+    with self.assertRaises(Exception):
+      plugin._delete_tcp_route(public_port=30000, expected_tunnel_id="tunnel-id")
+
+    self.assertEqual(plugin._chainstore[plugin.cfg_tcp_routes_hkey]["30000"]["tunnel_id"], "other-tunnel")
+    delete_writes = [call for call in plugin._chainstore_hsets if call["value"] is None]
+    self.assertEqual(delete_writes, [])
+
+    self.assertFalse(plugin._delete_tcp_route(public_port=30001, expected_tunnel_id="tunnel-id"))
+    delete_writes = [call for call in plugin._chainstore_hsets if call["value"] is None]
+    self.assertEqual(delete_writes, [])
+
+  def test_tcp_route_allocation_requires_chainstore_sync(self):
+    requests = _RequestsStub()
+    plugin = make_plugin(requests)
+
+    def failing_hsync(**kwargs):
+      raise Exception("sync failed")
+
+    plugin.chainstore_hsync = failing_hsync
+
+    with self.assertRaises(Exception) as ctx:
+      plugin._claim_tcp_route(
+        tunnel_id="tunnel-id",
+        hostname="uuid-001.ratio1.link",
+        alias="My TCP Tunnel",
+      )
+
+    self.assertIn("Could not sync TCP route registry", str(ctx.exception))
+    self.assertEqual(plugin._chainstore_hsets, [])
+
+  def test_tcp_alias_creates_origin_hostname_only(self):
+    requests = _RequestsStub(
+      get_payloads=[
+        {
+          "success": True,
+          "result": {
+            "id": "tunnel-id",
+            "metadata": {
+              "dns_name": "uuid-001.ratio1.link",
+              "custom_hostnames": [],
+              "type": "tcp",
+            },
+          },
+        },
+      ],
+      post_payloads=[
+        {
+          "success": True,
+          "result": {
+            "id": "alias-record-id",
+          },
+        },
+      ],
+      patch_payloads=[
+        {
+          "success": True,
+          "result": {},
+        },
+      ],
+    )
+    plugin = make_plugin(requests)
+
+    result = plugin.add_alias(
+      tunnel_id="tunnel-id",
+      alias="alias.ratio1.link",
+      cloudflare_account_id="account-id",
+      cloudflare_zone_id="zone-id",
+      cloudflare_api_key="api-key",
+      cloudflare_domain="ratio1.link",
+    )
+
+    self.assertTrue(result["success"])
+    self.assertEqual(len(requests.posts), 1)
+    self.assertEqual(requests.posts[0]["json"]["name"], "alias.ratio1.link")
+    aliases = requests.patches[0]["json"]["metadata"]["aliases"]
+    self.assertEqual(aliases[0]["public_id"], None)
+    self.assertEqual(aliases[0]["type"], "origin")
 
 
 if __name__ == "__main__":
